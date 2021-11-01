5c50f58baf0772e21b7dee581da8af3c8d65ec51
Jhon Camilo Baron Berdugo <jhooomn@gmail.com>
2021-11-01 15:18:58 -0500

    add: it was added vehicle model, serializers.py and endpoints

--- management/models.py
+++ management/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    vid = models.CharField(max_length=20)
+    type = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=15)
+    version = models.CharField(max_length=15)
+    licensePlate = models.CharField(max_length=15)
+    km = models.CharField(max_length=15)
+    imgLink = models.CharField(max_length=999)
+
+    class Meta:
+        db_table = "vehicle"
--- management/views.py
+++ management/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from management.models import Vehicle
+from management.serializers.serializers import VehicleSerializer
+
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Vehicle.objects.all().order_by('vid')
+    serializer_class = VehicleSerializer
